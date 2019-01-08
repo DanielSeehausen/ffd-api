3725530174a87650623b22a545eb818c4c6d6d3f
Daniel Seehausen <daniel.seehausen@gmail.com>
2019-01-08 13:20:35 -0800

    basic messaging pattern confirmed -- will use top level worker spin up top also manage the websocket connection updater

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ if (cluster.isMaster) {
     cluster.fork();
   }
   console.log(`\n\n ${cpus} workers loaded with config:\n\n`, config, '\n\n')
+  for (const id in cluster.workers) {
+    cluster.workers[id].send('test')
+  }
 } else {
   require('./src/worker/index.js')
 }
