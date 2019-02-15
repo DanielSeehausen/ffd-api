468a1c19c7fce26c104b939b17713c6efb02a1da
Daniel Seehausen <daniel.seehausen@gmail.com>
2019-02-15 08:49:28 -0500

    tests

--- test/requests/canvas.test.js
+++ test/requests/canvas.test.js
@@ -1,9 +1,25 @@
 const fetch = require('node-fetch')
 
-fetch('http://localhost:3000/canvas?id=0')
-  .then(res => { console.log(res); return res.arrayBuffer()})
-  .then(bufferData => {
-    console.log(bufferData)
-    const pixelArray = new Uint8ClampedArray(bufferData)
-    console.log('fetched canvas: ', pixelArray, "\n length:", pixelArray.length)
+const canvasEndpoint = 'http://localhost:3000/canvas?id=0'
+
+const fetchCanvas = () => fetch(canvasEndpoint).then(res => res.arrayBuffer())
+
+
+describe('canvas', () => {
+
+  test('returns an array buffer of the appropriate size', async () => {
+    const arrayBuffer = await fetchCanvas()
+    const pixelArray = new Uint32Array(arrayBuffer)
+
+    expect(pixelArray.length).toBe(4)
   })
+
+  test('the 4th color channel (alpha) is at max value (opaque)', async () => {
+    const arrayBuffer = await fetchCanvas()
+    const channelArray = new Uint8ClampedArray(arrayBuffer)
+    const alphaChannels = [channelArray[3], channelArray[7], channelArray[11], channelArray[15]]
+
+    alphaChannels.forEach(value => {expect(value).toBe(255)})
+  })
+
+})
