e09eb2dfc3e9a8ca074ef25d5f73c6ba56d38e8c
Daniel Seehausen <daniel.seehausen@gmail.com>
2019-02-19 12:10:00 +0100

    adds a test

--- test/requests/canvas.test.js
+++ test/requests/canvas.test.js
@@ -1,11 +1,18 @@
 const fetch = require('node-fetch')
 
 const canvasEndpoint = 'http://localhost:3000/canvas?id=0'
+const setTileEndpoint = 'http://localhost:3000/canvas?id=0'
 
 const fetchCanvas = () => fetch(canvasEndpoint).then(res => res.arrayBuffer())
 
 
 describe('canvas', () => {
+
+  test('returns a 200 response', async () => {
+    const res = await fetch(canvasEndpoint)
+
+    expect(res.status).toBe(200)
+  })
 
   test('returns an array buffer of the appropriate size', async () => {
     const arrayBuffer = await fetchCanvas()
@@ -20,6 +27,14 @@ describe('canvas', () => {
     const alphaChannels = [channelArray[3], channelArray[7], channelArray[11], channelArray[15]]
 
     alphaChannels.forEach(value => {expect(value).toBe(255)})
+  })
+
+  describe('setting tiles', () => {
+
+    test('returns an array buffer of the appropriate size', async () => {
+      expect(false).toBe(true)
+    })
+
   })
 
 })
