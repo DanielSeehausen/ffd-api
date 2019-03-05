374dd4a9d6fc93ed412f3800d1a7a809f3e70d34
Daniel Seehausen <daniel.seehausen@gmail.com>
2019-03-05 15:00:22 -0500

    fixes swap file oops add

--- src/models/Canvas.js
+++ src/models/Canvas.js
@@ -19,6 +19,7 @@ const Canvas = {
   create: () => {
     const arrayBuffer = createArrayBuffer()
     const int32View = new Uint32Array(arrayBuffer)
+    // TODO: move default color value to config
     const defaultColorValue = parseInt('FF000000', 16)
     int32View.fill(defaultColorValue) // hex6CharToInt32 helper not used because setting initial binary data requires alpha channel to be first. Now, when the data is read from redis in the get canvas request, it will come out '000000FF'. TL;DR: initial set from arrayBuffer is little endian, thereafter single tiles are set big endian
     const buffer = Buffer.from(arrayBuffer)
